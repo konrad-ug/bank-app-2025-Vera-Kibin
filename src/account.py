92da2ddb4abd87c82daef8baef7aaf7782fdaa80
Vera Kibin <verakibin@MacBook-Pro-Vera.local>
2025-10-16 17:07:39 +0200

    ADD: zadanie 5 - sprawdzanie PESEL; PROMO_KOD tylko dla osob z YoB > 1960

--- src/account.py
+++ src/account.py
@@ -1,10 +1,11 @@
+from datetime import date
 class Account:
     def __init__(self, first_name, last_name, pesel, promo_kod=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
         self.pesel = self.to_string_pesel(pesel)
-        self.promo_code = promo_kod if self.is_promo_valid(promo_kod) else None
+        self.promo_code = promo_kod if self.is_promo_valid(promo_kod) and self.is_rok_urodzienia_ok(self.pesel) else None
         if self.promo_code:
             self.balance += 50
 
@@ -18,6 +19,34 @@ class Account:
             return False
         res = str(pesel)
         return len(res) == 11 and all('0' <= c <= '9' for c in res)
+    
+    def yob_from_pesel(self, pesel):
+        sprawdzanie = str(pesel)
+        if len(sprawdzanie)!=11 or not sprawdzanie.isdigit():
+            return None
+        yy = int(sprawdzanie[0:2])
+        mm = int(sprawdzanie[2:4])
+        if 1 <= mm <= 12:
+            century = 1900
+        elif 21 <= mm <= 32:
+            century = 2000
+            mm -= 20
+        elif 41 <= mm <= 52:
+            century = 2100
+            mm -= 40
+        elif 61 <= mm <= 72:
+            century = 2200
+            mm -= 60
+        elif 81 <= mm <= 92:
+            century = 1800
+            mm -= 80
+        else:
+            return None 
+        return century + yy
+    
+    def is_rok_urodzienia_ok(self, pesel):
+        rok = self.yob_from_pesel(pesel)
+        return rok is not None and rok > 1960
     
     def is_promo_valid(self, promo_kod):
         if promo_kod is None:
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -46,51 +46,106 @@ class TestAccount:
         assert account.balance == 0
     
     def test_promo_maly_litery(self):
-        account = Account("Vera", "Kibin", 12345678910, "prom_hej")
+        account = Account("Vera", "Kibin", 81020311161, "prom_hej")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_promo_dobry(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_hej")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_hej")
         assert account.promo_code == "PROM_hej"
         assert account.balance == 50
 
     def test_promo_dobry_cyfry(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_123")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_123")
         assert account.promo_code == "PROM_123"
         assert account.balance == 50
 
     def test_promo_mixed_case(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_A9b")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_A9b")
         assert account.promo_code == "PROM_A9b"
         assert account.balance == 50
 
     def test_promo_zamalo_znakow(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_1")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_1")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_promo_zaduzo_znakow(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_112233")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_112233")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_niepoprawny_prefix(self):
-        account = Account("Vera", "Kibin", 12345678910, "BROM_123")
+        account = Account("Vera", "Kibin", 81020311161, "BROM_123")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_rozne_znaki(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_---")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_---")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_jeden_znak(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_12-")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_12-")
         assert account.promo_code == None
         assert account.balance == 0
 
     def test_promo_with_space_in_suffix(self):
-        account = Account("Vera", "Kibin", 12345678910, "PROM_1 2")
+        account = Account("Vera", "Kibin", 81020311161, "PROM_1 2")
         assert account.promo_code == None
         assert account.balance == 0
+
+## testy YoB
+
+    def test_promo_mixed_case_pesel_niepoprawny(self):
+        account = Account("Vera", "Kibin", 81020311161, "PROM_A9b")
+        assert account.promo_code == "PROM_A9b"
+        assert account.balance == 50
+
+    def test_promo_niepoprawny_pesel(self):
+        account = Account("Vera", "Kibin", 12345678919, "PROM_A9b")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_nieporawne_pesel_poprawny(self):
+        account = Account("Vera", "Kibin", 81020311161, "PROM_---")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_age_before_threshold_1959(self):
+        account = Account("Vera", "Kibin", "59030112345", "PROM_ABC")
+        assert account.promo_code is None
+        assert account.balance == 0
+
+    def test_promo_age_at_threshold_1960(self):
+        account = Account("Vera", "Kibin", "60030112345", "PROM_ABC")
+        assert account.promo_code is None
+        assert account.balance == 0
+
+    def test_promo_age_after_threshold_1961(self):
+        account = Account("Vera", "Kibin", "61030112345", "PROM_ABC")
+        assert account.promo_code == "PROM_ABC"
+        assert account.balance == 50
+
+    def test_promo_age_2001_example(self):
+        account = Account("Vera", "Kibin", "01210512345", "PROM_X9z")
+        assert account.promo_code == "PROM_X9z"
+        assert account.balance == 50
+
+    def test_promo_age_1899_not_eligible(self):
+        account = Account("Vera", "Kibin", "99920112345", "PROM_ABC")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_blocked_when_pesel_invalid(self):
+        account = Account("Vera", "Kibin", "1234567ABCD", "PROM_ABC") 
+        assert account.pesel == "Invalid"
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_blocked_when_month_invalid(self):
+        account = Account("Vera", "Kibin", "61000112345", "PROM_ABC")
+        assert account.promo_code is None
+        assert account.balance == 0
+
+
