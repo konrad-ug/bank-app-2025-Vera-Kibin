cd2cf4dd9fed1bc64aa00cae1d1164b67eb7eba3
Vera Kibin <verakibin@MacBook-Pro-Vera.local>
2025-10-16 16:28:30 +0200

    ADD: zadanie 4 - PROMO_KOD

--- src/account.py
+++ src/account.py
@@ -1,9 +1,12 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel):
+    def __init__(self, first_name, last_name, pesel, promo_kod=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
         self.pesel = self.to_string_pesel(pesel)
+        self.promo_code = promo_kod if self.is_promo_valid(promo_kod) else None
+        if self.promo_code:
+            self.balance += 50
 
     def to_string_pesel(self, pesel):
         if self.is_pesel_valid(pesel):
@@ -15,4 +18,11 @@ class Account:
             return False
         res = str(pesel)
         return len(res) == 11 and all('0' <= c <= '9' for c in res)
+    
+    def is_promo_valid(self, promo_kod):
+        if promo_kod is None:
+            return False
+        res = str(promo_kod)
+        suffix = res[len("PROM_"):]
+        return res.startswith("PROM_") and len(res) == 8 and all(c.isalnum() for c in suffix)
            
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,6 +8,10 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678910"
+        assert account.promo_code == None
+
+## testy PESEL
+
     def test_pesel_empty(self):
         account = Account("Vera", "Kibin", "")
         assert account.pesel == "Invalid"
@@ -29,3 +33,64 @@ class TestAccount:
     def test_pesel_typ_digit(self):
         account = Account("Vera", "Kibin", 12345678910)
         assert account.pesel == "12345678910"
+    def test_pesel_z_zero(self):
+        account = Account("Vera", "Kibin", "01234567890")
+        assert account.pesel == "01234567890"
+
+
+## testy PROMO_KOD
+
+    def test_promo_none(self):
+        account = Account("Vera", "Kibin", 12345678910)
+        assert account.promo_code == None
+        assert account.balance == 0
+    
+    def test_promo_maly_litery(self):
+        account = Account("Vera", "Kibin", 12345678910, "prom_hej")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_dobry(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_hej")
+        assert account.promo_code == "PROM_hej"
+        assert account.balance == 50
+
+    def test_promo_dobry_cyfry(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_123")
+        assert account.promo_code == "PROM_123"
+        assert account.balance == 50
+
+    def test_promo_mixed_case(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_A9b")
+        assert account.promo_code == "PROM_A9b"
+        assert account.balance == 50
+
+    def test_promo_zamalo_znakow(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_1")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_zaduzo_znakow(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_112233")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_niepoprawny_prefix(self):
+        account = Account("Vera", "Kibin", 12345678910, "BROM_123")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_rozne_znaki(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_---")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_jeden_znak(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_12-")
+        assert account.promo_code == None
+        assert account.balance == 0
+
+    def test_promo_with_space_in_suffix(self):
+        account = Account("Vera", "Kibin", 12345678910, "PROM_1 2")
+        assert account.promo_code == None
+        assert account.balance == 0
