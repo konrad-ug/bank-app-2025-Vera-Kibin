8650b8110e7419a0c9fbd0c03a107bc0e7975e1a
Vera Kibin <verakibin@MacBook-Pro-Vera.local>
2025-10-16 15:33:24 +0200

    ADD: zadanie 3 - sprawdanie PESEL

--- src/account.py
+++ src/account.py
@@ -3,9 +3,16 @@ class Account:
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
-        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        self.pesel = self.to_string_pesel(pesel)
+
+    def to_string_pesel(self, pesel):
+        if self.is_pesel_valid(pesel):
+            return str(pesel)
+        return "Invalid"
 
     def is_pesel_valid(self, pesel):
-        if pesel and len(pesel) == 11:
-            return True
-        return False
+        if pesel is None:
+            return False
+        res = str(pesel)
+        return len(res) == 11 and all('0' <= c <= '9' for c in res)
+           
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,3 +8,24 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678910"
+    def test_pesel_empty(self):
+        account = Account("Vera", "Kibin", "")
+        assert account.pesel == "Invalid"
+    def test_pesel_empty_none(self):
+        account = Account("Vera", "Kibin", None)
+        assert account.pesel == "Invalid"
+    def test_pesel_less(self):
+        account = Account("Vera", "Kibin", "123")
+        assert account.pesel == "Invalid"
+    def test_pesel_more(self):
+        account = Account("Vera", "Kibin", "123123123123")
+        assert account.pesel == "Invalid"
+    def test_pesel_litera(self):
+        account = Account("Vera", "Kibin", "1234567891A")
+        assert account.pesel == "Invalid"
+    def test_pesel_litery(self):
+        account = Account("Vera", "Kibin", "mlkhgfedcbA")
+        assert account.pesel == "Invalid"
+    def test_pesel_typ_digit(self):
+        account = Account("Vera", "Kibin", 12345678910)
+        assert account.pesel == "12345678910"
